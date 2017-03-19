95b3e55a971e1b3611e7ca0ddb299f81e1ba2385
Thatsmusic99 <tmtherobotcat@gmail.com>
2017-03-19 17:53:12 +0000

    Constantly making changes and fixes.

--- main/java/io/github/thatsmusic99/headsplus/HeadsPlusConfigHeads.java
+++ main/java/io/github/thatsmusic99/headsplus/HeadsPlusConfigHeads.java
@@ -10,6 +10,8 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 public class HeadsPlusConfigHeads {
+	public static List<String> mHeads = new ArrayList<>(Arrays.asList("blaze", "cavespider", "chicken", "cow", "creeper", "enderman", "ghast", "guardian", "irongolem", "mushroomcow", "pig", "sheep", "skeleton", "slime", "spider", "squid", "villager", "witch", "zombie"));
+	public static List<String> uHeads = new ArrayList<>(Arrays.asList("bat", "donkey", "enderdragon", "elderguardian", "endermite", "evoker", "horse", "llama", "magmacube", "mule", "polarbear", "rabbit", "shulker", "silverfish", "skeletonhorse", "snowman", "stray", "vex", "vindicator", "wither", "witherskeleton"));
 	private static FileConfiguration heads;
 	public static File headsF;
 	
@@ -46,7 +48,7 @@ public class HeadsPlusConfigHeads {
     	}
     }
     public static void addUndefinedHeads() {
-    	List<String> uHeads = new ArrayList<>(Arrays.asList("bat", "donkey", "enderdragon", "elderguardian", "endermite", "evoker", "horse", "llama", "magmacube", "mule", "polarbear", "rabbit", "shulker", "silverfish", "skeletonhorse", "snowman", "stray", "vex", "vindicator", "wither", "witherskeleton"));
+    	
     	for (String key : uHeads) {
     		getHeads().addDefault(key + "HeadN", "");
     		getHeads().addDefault(key + "HeadC", 0);
@@ -55,7 +57,7 @@ public class HeadsPlusConfigHeads {
     	}
     }
     public static void addMHFHeads() {
-    	List<String> mHeads = new ArrayList<>(Arrays.asList("blaze", "cavespider", "chicken", "cow", "creeper", "enderman", "ghast", "guardian", "irongolem", "mushroomcow", "pig", "sheep", "skeleton", "slime", "spider", "squid", "villager", "witch", "zombie"));
+    	
     	for (String key : mHeads) {
     		if (key != "irongolem") {
     		    getHeads().addDefault(key + "HeadN", "MHF_" + key);
@@ -63,10 +65,12 @@ public class HeadsPlusConfigHeads {
     		    String str = key.substring(0, 1).toUpperCase();
     		    String str2 = key.substring(1);
     		    getHeads().addDefault(key + "HeadDN", str + str2 + " Head");
+    		    getHeads().addDefault(key + "HeadP", 10.00);
     		} else {
     			getHeads().addDefault("irongolemHeadN", "MHF_Golem");
     			getHeads().addDefault("irongolemHeadC", 25);
     			getHeads().addDefault("irongolemHeadDN", "Iron Golem Head");
+    			getHeads().addDefault("irongolemHeadP", 10.00);
     		}
     	}
     }
--- main/java/io/github/thatsmusic99/headsplus/commands/SellHead.java
+++ main/java/io/github/thatsmusic99/headsplus/commands/SellHead.java
@@ -1,5 +1,7 @@
 package io.github.thatsmusic99.headsplus.commands;
 
+import java.util.List;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -28,26 +30,68 @@ public class SellHead implements CommandExecutor {
 		    	ItemStack skull = new ItemStack(Material.SKULL_ITEM);
 		        SkullMeta skullM = (SkullMeta) invi.getItemMeta();
 		        String owner = skullM.getOwner();
-		    	if (skullM.getLore() != null && skullM.getLore().get(0) == " ") {
+		    	if ((skullM.getLore() != null) && (skullM.getLore().get(0) == " ")) {
 		    		Economy econ = null;
-		    		if (owner == HeadsPlusConfigHeads.getHeads().getString("zombieHeadN")) {
-		    			Double price = HeadsPlusConfigHeads.getHeads().getDouble("zombieHeadP");
-		    			String senderName = sender.getName();
-						@SuppressWarnings({ "deprecation", "null" })
-						EconomyResponse zr = econ.depositPlayer(senderName, price);
-						String success = HeadsPlus.getInstance().translateMessages(HeadsPlusConfig.getMessages().getString("sell-success"));
-						success = ChatColor.translateAlternateColorCodes('&', success);
-						String fail = HeadsPlus.getInstance().translateMessages(HeadsPlusConfig.getMessages().getString("sell-fail"));
-						fail = ChatColor.translateAlternateColorCodes('&', fail);
-						if (zr.transactionSuccess()) {
-							((Player) sender).getInventory().remove(skull);
-							sender.sendMessage(success);
-						} else {
-							sender.sendMessage(fail + ": " + zr.errorMessage);
-						}
-		    		} else {
-		    		    sender.sendMessage("Ping"); // TODO Add undefined message.
-		    	    }
+		    		List<String> mHeads = HeadsPlusConfigHeads.mHeads;
+		    		List<String> uHeads = HeadsPlusConfigHeads.uHeads; 
+		    		for (String key : mHeads) {
+		    			
+		    			if (owner.matches(HeadsPlusConfigHeads.getHeads().getString(key + "HeadN"))) {
+		    				String senderName = sender.getName();
+		    				Double price = HeadsPlusConfigHeads.getHeads().getDouble(key + "HeadP");
+		    				@SuppressWarnings({ "deprecation", "null" })
+							EconomyResponse zr = econ.depositPlayer(senderName, price);
+		    				String success = HeadsPlus.getInstance().translateMessages(HeadsPlusConfig.getMessages().getString("sell-success"));
+							success = ChatColor.translateAlternateColorCodes('&', success);
+							String fail = HeadsPlus.getInstance().translateMessages(HeadsPlusConfig.getMessages().getString("sell-fail"));
+							fail = ChatColor.translateAlternateColorCodes('&', fail);
+							if (zr.transactionSuccess()) {
+								((Player) sender).getInventory().remove(skull);
+								sender.sendMessage(success);
+							} else {
+								sender.sendMessage(fail + ": " + zr.errorMessage);
+							}
+		    				
+		    			} else if ((owner.matches("MHF_Golem")) && (key.equalsIgnoreCase("irongolem"))) {
+		    				Double price = HeadsPlusConfigHeads.getHeads().getDouble("irongolemHeadP");
+		    				String senderName = sender.getName();
+		    				@SuppressWarnings({ "deprecation", "null" })
+							EconomyResponse zr = econ.depositPlayer(senderName, price);
+		    				String success = HeadsPlus.getInstance().translateMessages(HeadsPlusConfig.getMessages().getString("sell-success"));
+							success = ChatColor.translateAlternateColorCodes('&', success);
+							String fail = HeadsPlus.getInstance().translateMessages(HeadsPlusConfig.getMessages().getString("sell-fail"));
+							fail = ChatColor.translateAlternateColorCodes('&', fail);
+							if (zr.transactionSuccess()) {
+								((Player) sender).getInventory().remove(skull);
+								sender.sendMessage(success);
+							} else {
+								sender.sendMessage(fail + ": " + zr.errorMessage);
+								sender.sendMessage(key);
+								
+							}
+		    			}
+		    		}
+		    		for (String key : uHeads) {
+		    			if (owner.matches(HeadsPlusConfigHeads.getHeads().getString(key + "HeadN"))) {
+		    				String senderName = sender.getName();
+		    				Double price = HeadsPlusConfigHeads.getHeads().getDouble(key + "HeadP");
+		    				@SuppressWarnings({ "deprecation", "null" })
+							EconomyResponse zr = econ.depositPlayer(senderName, price);
+		    				String success = HeadsPlus.getInstance().translateMessages(HeadsPlusConfig.getMessages().getString("sell-success"));
+							success = ChatColor.translateAlternateColorCodes('&', success);
+							String fail = HeadsPlus.getInstance().translateMessages(HeadsPlusConfig.getMessages().getString("sell-fail"));
+							fail = ChatColor.translateAlternateColorCodes('&', fail);
+							if (zr.transactionSuccess()) {
+								((Player) sender).getInventory().remove(skull);
+								sender.sendMessage(success);
+							} else {
+								sender.sendMessage(fail + ": " + zr.errorMessage);
+							}
+		    				
+		    			} else {
+		    				sender.sendMessage("You failure.");
+		    			}
+		    		}
 		    	} else {
 		    		String falseHead = HeadsPlus.getInstance().translateMessages(HeadsPlusConfig.getMessages().getString("false-head"));
 		    		falseHead = ChatColor.translateAlternateColorCodes('&', falseHead);

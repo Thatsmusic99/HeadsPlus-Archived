4e166d10ed4cbcf40884ad8ee83a2d51950a15a2
Thatsmusic99 <tmtherobotcat@gmail.com>
2017-03-23 19:18:26 +0000

    Switching to TreeMap

--- main/java/io/github/thatsmusic99/headsplus/events/BlockEvent.java
+++ main/java/io/github/thatsmusic99/headsplus/events/BlockEvent.java
@@ -2,6 +2,7 @@ package io.github.thatsmusic99.headsplus.events;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.TreeMap;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -9,14 +10,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
 public class BlockEvent implements Listener { // BlockEvents, according to Eclipse, doesn't exist.
 	
 	ArrayList<String[]> skullList;
-	HashMap<Location, SkullMeta> skullmap = new HashMap<Location, SkullMeta>();
+	TreeMap<Location, SkullMeta> skullmap = new TreeMap<Location, SkullMeta>();
 	Location skullLoc;
 	SkullMeta skullMeta;
 	
@@ -26,26 +26,20 @@ public class BlockEvent implements Listener { // BlockEvents, according to Eclip
 		    ItemStack skull = e.getItemInHand();
 		    SkullMeta skullM = (SkullMeta) skull.getItemMeta();
 		    Location skullL = e.getBlock().getLocation();
-		    skullMeta = skullM;
 		    skullmap.put(skullL, skullM);
+		    skullMeta = skullM;
 		}
 	}
 	@EventHandler 
 	public void onBlockBreak(BlockBreakEvent e) {
 		if (e.getBlock().getType() == Material.SKULL) {
 			Location skullL = e.getBlock().getLocation();
-			if (skullmap.containsKey(skullL)) {
-				skullLoc = skullL;
-			}
-		}
-	}
-	
-	@EventHandler
-	public void onItemPickup(PlayerPickupItemEvent e) {
-		if (e.getItem().getItemStack().getType() == Material.SKULL_ITEM) {
-			if ((skullmap.containsKey(skullLoc)) && (skullmap.containsKey(skullMeta))) {
-				ItemStack skull = e.getItem().getItemStack();
+			if (skullmap.containsValue(skullL)) {
+				
+				ItemStack skull = new ItemStack(e.getBlock().getType());
 				skull.setItemMeta(skullMeta);
+				
+				
 			}
 		}
 	}

e1cb2f3c03155e48a1f8ab22905845171cd3091d
Thatsmusic99 <tmtherobotcat@gmail.com>
2017-04-04 09:53:43 +0100

    Bumped to 0.9.0 (untested)
    - Added enums for adding recipes
    - Added crafting.yml

--- main/java/io/github/thatsmusic99/headsplus/config/HeadsPlusCrafting.java
+++ main/java/io/github/thatsmusic99/headsplus/config/HeadsPlusCrafting.java
@@ -1,15 +1,23 @@
 package io.github.thatsmusic99.headsplus.config;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
+
+import io.github.thatsmusic99.headsplus.HeadsPlus;
 
 public class HeadsPlusCrafting {
 	
+	private static List<String> mHeads = HeadsPlusConfigHeads.mHeads;
+	private static List<String> uHeads = HeadsPlusConfigHeads.uHeads;
+	
+	private static List<ItemStack> blazeI = new ArrayList<>();
 	private static List<ItemStack> zombieI = new ArrayList<>();
 	private static List<ItemStack> skeleI = new ArrayList<>();
 	
@@ -19,16 +27,58 @@ public class HeadsPlusCrafting {
 	public static FileConfiguration getCrafting() {
 		return crafting;
 	}
+	public static void craftingEnable() {
+		reloadCrafting();
+		loadCrafting();
+		checkCrafting();
+		reloadCrafting();
+	}
 	
 	private static void loadCrafting() {
 		getCrafting().options().header("HeadsPlus by Thatsmusic99");
+		getCrafting().addDefault("blazeI", blazeI);
 		getCrafting().addDefault("zombieI", zombieI);
-		
+		getCrafting().addDefault("skeleI", skeleI);
 	}
 	public static void addIngredients() {
+		ItemStack blazeIt = new ItemStack(Material.BLAZE_POWDER);
+		blazeI.add(blazeIt);
 		ItemStack zombieIt = new ItemStack(Material.ROTTEN_FLESH);
 		zombieI.add(zombieIt);
 		ItemStack skeleIt = new ItemStack(Material.BONE);
-		
+		skeleI.add(skeleIt);
+	}
+	public static void reloadCrafting() {
+		if (craftingF == null) {
+			craftingF = new File(HeadsPlus.getInstance().getDataFolder(), "crafting.yml");
+		}
+		crafting = YamlConfiguration.loadConfiguration(craftingF);
+		loadCrafting();
+		saveCrafting();
+	}
+	public static void saveCrafting() {
+		if (crafting == null || craftingF == null) {
+			return;
+		}
+		try {
+			crafting.save(craftingF);
+		} catch (IOException e) {
+			HeadsPlus.getInstance().log.severe("[HeadsPlus] Couldn't save crafting.yml!");
+			e.printStackTrace();
+		}
+	}
+	public static void checkCrafting() {
+		for (String key : mHeads) {
+			List<String> keyl = getCrafting().getStringList(key + "I");
+			if (keyl.size() > 9) {
+				getCrafting().getStringList(key + "I").clear();
+			}
+		}
+		for (String key : uHeads) {
+			List<String> keyl = getCrafting().getStringList(key + "I");
+			if (keyl.size() > 9) {
+				getCrafting().getStringList(key + "I").clear();
+			}
+		}
 	}
 }
---
+++ main/java/io/github/thatsmusic99/headsplus/crafting/RecipeEnumUser.java
@@ -0,0 +1,38 @@
+package io.github.thatsmusic99.headsplus.crafting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import io.github.thatsmusic99.headsplus.config.HeadsPlusConfigHeads;
+import io.github.thatsmusic99.headsplus.config.HeadsPlusCrafting;
+
+public class RecipeEnumUser {
+	
+	private static FileConfiguration crafting = HeadsPlusCrafting.getCrafting();
+	static RecipeEnums enums;
+	
+	public void addEnumToConfig() {
+		for (RecipeEnums key : RecipeEnums.values()) {
+			ItemStack i = new ItemStack(Material.SKULL_ITEM, 1, (byte) 3);
+			SkullMeta im = (SkullMeta) i.getItemMeta();
+			im.setDisplayName(HeadsPlusConfigHeads.getHeads().getString(key.str + "HeadDN"));
+			im.setOwner(HeadsPlusConfigHeads.getHeads().getString(key.str + "HeadN"));
+			RecipeListeners.makeSell(im);
+			i.setItemMeta(im);
+			ShapelessRecipe recipe = new ShapelessRecipe(i)
+					.addIngredient(key.mat);
+			Bukkit.addRecipe(recipe);
+			List<String> ingrs = new ArrayList<>();
+			ingrs.add(key.mat.toString());
+			crafting.addDefault(key.str + "I", ingrs);
+		}
+	}
+
+}

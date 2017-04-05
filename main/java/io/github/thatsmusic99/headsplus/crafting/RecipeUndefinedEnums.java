acd528fd3805105c9a730405c636b31d7f2b983b
Thatsmusic99 <tmtherobotcat@gmail.com>
2017-04-05 07:54:50 +0100

    Added undefined enums

--- main/java/io/github/thatsmusic99/headsplus/crafting/RecipeUndefinedEnums.java
+++ main/java/io/github/thatsmusic99/headsplus/crafting/RecipeUndefinedEnums.java
@@ -1,5 +1,12 @@
 package io.github.thatsmusic99.headsplus.crafting;
 
 public enum RecipeUndefinedEnums {
+	
+	BAT("bat"), DONKEY("donkey"), ENDERDRAGON("enderdragon"), ELDERGUARDIAN("elderguardian"), ENDERMITE("endermite"), EVOKER("evoker"), HORSE("horse"), LLAMA("llama"), MAGMACUBE("magmacube"), MULE("mule"), POLARBEAR("polarbear"), RABBIT("rabbit"), SHULKER("shulker"), SILVERFISH("silverfish"), SKELETONHORSE("skeletonhorse"), SNOWMAN("snowman"), STRAY("stray"), VEX("vex"), VINDICATOR("vindicator"), WITHER("wither"), WITHERSKELETON("witherskeleton");
+	String str;
+	
+	private RecipeUndefinedEnums(String str) {
+		this.str = str;
+	}
 
 }

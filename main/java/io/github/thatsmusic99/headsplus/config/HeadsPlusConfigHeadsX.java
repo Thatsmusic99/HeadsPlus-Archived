583af8218beb8e30512c430571e0cce617f30e20
Thatsmusic99 <tmtherobotcat@gmail.com>
2017-09-09 07:57:05 +0100

    I've made a mistake

--- main/java/io/github/thatsmusic99/headsplus/config/HeadsPlusConfigHeadsX.java
+++ main/java/io/github/thatsmusic99/headsplus/config/HeadsPlusConfigHeadsX.java
@@ -8,6 +8,8 @@ import java.io.File;
 import java.io.IOException;
 
 public class HeadsPlusConfigHeadsX {
+
+    private static boolean s = false;
     private static FileConfiguration headsx;
     private static File headsxf;
 
@@ -26,7 +28,12 @@ public class HeadsPlusConfigHeadsX {
         getHeadsX().options().header("HeadsPlus by Thatsmusic99 " +
                 "\n WARNING: This is an advanced section of the plugin. If you do not know what you a doing with it, please do not use it due to risk of crashing your own and other's games. " +
                 "\n For more information visit the GitHub wiki for HeadsX.yml: ");
-
+        for (HeadsXEnums e : HeadsXEnums.values()) {
+            getHeadsX().addDefault("heads." + e.name + ".database", true);
+            getHeadsX().addDefault("heads." + e.name + ".encode", false);
+            getHeadsX().addDefault("heads." + e.name + ".displayname", e.dn);
+            getHeadsX().addDefault("heads." + e.name + ".texture", e.tex);
+        }
         saveHeadsX();
     }
     public static FileConfiguration getHeadsX() {
@@ -35,13 +42,15 @@ public class HeadsPlusConfigHeadsX {
     public static void reloadHeadsX() {
         if (headsxf == null) {
             headsxf = new File(HeadsPlus.getInstance().getDataFolder(), "headsx.yml");
+            s = true;
         }
         headsx = YamlConfiguration.loadConfiguration(headsxf);
-        loadHeadsX();
         saveHeadsX();
     }
     public static void headsxEnable() {
         reloadHeadsX();
-        loadHeadsX();
+        if (s) {
+            loadHeadsX();
+        }
     }
 }

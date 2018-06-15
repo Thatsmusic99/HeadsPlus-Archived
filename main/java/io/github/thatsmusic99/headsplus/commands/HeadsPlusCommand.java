86f4501f4a8ec2155a5da04b57d81c5ca6c4545b
Thatsmusic99 <tmtherobotcat@gmail.com>
2018-06-15 18:55:02 +0100

    Fixed error messages not coming up + advanced uses

--- main/java/io/github/thatsmusic99/headsplus/commands/HeadsPlusCommand.java
+++ main/java/io/github/thatsmusic99/headsplus/commands/HeadsPlusCommand.java
@@ -26,7 +26,14 @@ public class HeadsPlusCommand implements CommandExecutor {
                                 if (command.isCorrectUsage(args, sender).get(true) != null) {
                                     return command.fire(args, sender);
                                 } else {
+                                    sender.sendMessage(command.isCorrectUsage(args, sender).get(false));
                                     sender.sendMessage(ChatColor.DARK_RED + "Usage: " + ChatColor.RED + command.getUsage());
+                                    if (command.advancedUsages().length != 0) {
+                                        sender.sendMessage(ChatColor.DARK_RED + "Further usages:");
+                                        for (String s : command.advancedUsages()) {
+                                            sender.sendMessage(ChatColor.RED + s);
+                                        }
+                                    }
                                 }
                             } else {
                                 new HelpMenu().fire(args, sender);

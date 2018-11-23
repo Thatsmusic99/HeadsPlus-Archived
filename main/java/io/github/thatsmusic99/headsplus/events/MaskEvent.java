4e06940e9f87dd630bb7a1d4d08f8957ec5cee0e
Thatsmusic99 <tmtherobotcat@gmail.com>
2018-11-23 12:52:56 +0000

    Seriously trying to fix this...

--- main/java/io/github/thatsmusic99/headsplus/events/MaskEvent.java
+++ main/java/io/github/thatsmusic99/headsplus/events/MaskEvent.java
@@ -7,7 +7,6 @@ import io.github.thatsmusic99.headsplus.nms.NMSManager;
 import io.github.thatsmusic99.headsplus.nms.v1_8_R1_NMS.v1_8_R1_NMS;
 import io.github.thatsmusic99.headsplus.nms.v1_8_R2_NMS.v1_8_R2_NMS;
 import io.github.thatsmusic99.headsplus.nms.v1_8_R3_NMS.v1_8_R3NMS;
-import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -17,7 +16,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.HashMap;
-import java.util.List;
 
 public class MaskEvent implements Listener {
 
@@ -31,10 +29,10 @@ public class MaskEvent implements Listener {
             if (e.getSlot() == 39
                     || ((nms instanceof v1_8_R3NMS
                     || nms instanceof v1_8_R2_NMS || nms instanceof v1_8_R1_NMS) && e.getRawSlot() == 5)) {
+
                 ItemStack ist = e.getCursor();
                 if (ist != null) {
-
-                    if (ist.getType().equals(nms.getSkullMaterial(1).getType())) {
+                    if (ist.getType().equals(nms.getSkull(3).getType())) {
                         HeadsPlusConfigHeads hpch = hp.getHeadsConfig();
                         String s = nms.getType(ist).toLowerCase();
                         if (hpch.mHeads.contains(s) || hpch.uHeads.contains(s) || s.equalsIgnoreCase("player")) {
@@ -43,8 +41,8 @@ public class MaskEvent implements Listener {
                             maskMonitors.put((Player) e.getWhoClicked(), new BukkitRunnable() {
                                 @Override
                                 public void run() {
-                                    if (e.getWhoClicked().getInventory().getItem(39) == null || ((nms instanceof v1_8_R3NMS
-                                            || nms instanceof v1_8_R2_NMS || nms instanceof v1_8_R1_NMS) && e.getWhoClicked().getInventory().getItem(5) == null)) {
+                                    if (e.getWhoClicked().getInventory().getItem(39) == null && !((nms instanceof v1_8_R3NMS
+                                            || nms instanceof v1_8_R2_NMS || nms instanceof v1_8_R1_NMS) && e.getWhoClicked().getInventory().getItem(5) != null)) {
                                         HPPlayer pl = HPPlayer.getHPPlayer((OfflinePlayer) e.getWhoClicked());
                                         pl.clearMask();
                                         maskMonitors.remove(e.getWhoClicked());

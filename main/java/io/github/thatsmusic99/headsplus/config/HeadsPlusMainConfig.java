a3eec125c24951b5bbd4cd876ca6efb57bc5158e
Thatsmusic99 <tmtherobotcat@gmail.com>
2019-02-15 19:48:22 +0000

    ^ part 2

--- main/java/io/github/thatsmusic99/headsplus/config/HeadsPlusMainConfig.java
+++ main/java/io/github/thatsmusic99/headsplus/config/HeadsPlusMainConfig.java
@@ -64,6 +64,7 @@ public class HeadsPlusMainConfig extends ConfigSettings {
         getConfig().addDefault("plugin.mechanics.update.check", true);
         getConfig().addDefault("plugin.mechanics.update.notify", true);
         getConfig().addDefault("plugin.mechanics.allow-looting-enchantment", true);
+        getConfig().addDefault("plugin.mechanics.looting.ignored-entities", new ArrayList<>());
         getConfig().addDefault("plugin.mechanics.stop-placement-of-sellable-heads", false);
         getConfig().addDefault("plugin.mechanics.sellhead-gui", true);
         getConfig().addDefault("plugin.mechanics.debug.create-debug-files", true);

138b6ee262c00f5213a8f293e4f54883f7e216a0
Thatsmusic99 <tmtherobotcat@gmail.com>
2019-05-25 08:13:12 +0100

    Small code cleanup

--- main/java/io/github/thatsmusic99/headsplus/util/InventoryManager.java
+++ main/java/io/github/thatsmusic99/headsplus/util/InventoryManager.java
@@ -22,16 +22,16 @@ import java.util.logging.Level;
 
 public class InventoryManager {
 
-    public static enum Type {
+    public enum Type {
         SELL, LIST_MENU, LIST_CATEGORY, LIST_SEARCH, LIST_FAVORITES, CHALLENGES_MENU, CHALLENGES_LIST
     }
 
-    public static enum Page {
+    public enum Page {
 
         MENU('M'), START('<'), BACK('B'), NEXT('N'), BACK_2('['), BACK_3('{'), NEXT_2(']'), NEXT_3('}'), LAST('>');
         public final char shortHand;
 
-        private Page(char shortHand) {
+        Page(char shortHand) {
             this.shortHand = shortHand;
         }
     }
@@ -211,7 +211,7 @@ public class InventoryManager {
             max = (plugin.getItems().getConfig().getInt("inventories.headmenu.size") - 9);
             wide = true;
         }
-        List<ItemStack> heads = new ArrayList();
+        List<ItemStack> heads = new ArrayList<>();
         int start = currentPage * max;
         String[] sections = hpchx.sections.keySet().toArray(new String[0]);
         int categories = sections.length;
@@ -269,7 +269,7 @@ public class InventoryManager {
             wide = true;
         }
 
-        HashMap<String, String> s = new HashMap();
+        HashMap<String, String> s = new HashMap<>();
         for (String o : hpch.mHeads) {
             if (!hpch.getConfig().getStringList(o + ".name").isEmpty()) {
                 s.put(o, o + ".name");
